6569cddcee808f23a7c28489c4d7f29c8b896136
Daniel Gallardo <daniel.gallardo@alterbios.com>
2020-01-02 14:29:49 -0500

    Lectura de facturas con <![CDATA[]]> mostrar información de cada factura al seleccionarla

--- AnalisisProductos/AnalisisProductos-web/src/main/java/ec/edu/espe/tesis/beans/HttpSessionHandler.java
+++ AnalisisProductos/AnalisisProductos-web/src/main/java/ec/edu/espe/tesis/beans/HttpSessionHandler.java
@@ -5,7 +5,6 @@ import ec.edu.espe.tesis.facturas.model.Usuario;
 import java.io.IOException;
 import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
 import org.omnifaces.util.Faces;
@@ -14,27 +13,13 @@ import org.omnifaces.util.Faces;
  *
  * @author alterbios
  */
-@Named("session")//para poder usar desde UI con el nombre de sesionQF
+@Named("sessionBean")//para poder usar desde UI con el nombre de sesionQF
 @SessionScoped
 public class HttpSessionHandler implements Serializable {
 
     private HttpSession session;
-    private String idUsuario = "";
-    private String idPerfil = "";
-    private String identificacionUsuario = "";
-    private String menuXHTML = "";
-    private String nombreUsuario = "";
-    private String nombrePerfil = "";
-    
-
-
-    public String getNombreUsuario() {
-        return nombreUsuario;
-    }
-
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
-    }
+    private String correo=  "";
+ 
 
     public HttpSession getSession() {
         return session;
@@ -44,77 +29,45 @@ public class HttpSessionHandler implements Serializable {
         this.session = session;
     }
 
-    public String getIdPerfil() {
-        return idPerfil;
+    public String getCorreo() {
+        return correo;
     }
 
-    public void setIdPerfil(String idPerfil) {
-        this.idPerfil = idPerfil;
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 
-    public String getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(String idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getIdentificacionUsuario() {
-        return identificacionUsuario;
-    }
-
-    public void setIdentificacionUsuario(String identificacionUsuario) {
-        this.identificacionUsuario = identificacionUsuario;
-    }
-
-    public String getMenuXHTML() {
-        return menuXHTML;
-    }
-
-    public void setMenuXHTML(String menuXHTML) {
-        this.menuXHTML = menuXHTML;
-    }
-
-    public String getNombrePerfil() {
-        return nombrePerfil;
-    }
-
-    public void setNombrePerfil(String nombrePerfil) {
-        this.nombrePerfil = nombrePerfil;
-    }
-
-    public void login(String idUsuario, String idPerfil, HttpSession _session) {
+    public void login(String idUsuario, HttpSession _session) {
         setSession(_session);
         session.setMaxInactiveInterval(60 * 60 * 6);
-        setIdUsuario(idUsuario);
+        setCorreo(idUsuario);
        // setIdPerfil(idPerfil);
         
     }
 
-    public void loginReceptor(String idUsuario, String idPerfil, String identificacion, HttpSession _session) {
+    public void loginReceptor(String idUsuario, HttpSession _session) {
         setSession(session);
         session.setMaxInactiveInterval(60 * 60 * 6);
-        setIdUsuario(idUsuario);
-        setIdentificacionUsuario(identificacion);
+        setCorreo(idUsuario);
+      //  setIdentificacionUsuario(identificacion);
        // setIdPerfil(idPerfil);
     }
 
     public boolean isLoggedIn() {
-        return session != null && idUsuario != null && !idUsuario.isEmpty();
+        return session != null && correo != null && !correo.isEmpty();
     }
 
     public void cerrarSesion() {
-        Usuario u = new Usuario(Integer.valueOf(idUsuario));
+       // Usuario u = new Usuario(Integer.valueOf(correo));
         
-        session.invalidate();
-        idUsuario = null;
-        session = null;
-        try {
-            Faces.redirect("login.xhtml?faces-redirect=true");
-        } catch (IOException ex) {
-            System.out.println("No se puede cerrar la session " + ex);
-        }
+//        session.invalidate();
+//        correo = null;
+//        session = null;
+//        try {
+//            Faces.redirect("login.xhtml?faces-redirect=true");
+//        } catch (IOException ex) {
+//            System.out.println("No se puede cerrar la session " + ex);
+//        }
     }
 
 }
